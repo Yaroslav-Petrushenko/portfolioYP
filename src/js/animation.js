467d23e13e33584f8139beacdeaf01c17e5a306d
Yaroslav-Petrushenko <igorovica@gmail.com>
2023-02-26 12:38:42 +0200

    span text animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,3 +1,7 @@
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // animation
 const animItems = document.querySelectorAll(`._anim-start`)
 if (animItems.length > 0) {
@@ -32,8 +36,56 @@ if (animItems.length > 0) {
             left: rect.left + scrollLeft
         }
     }
-    
+
     setTimeout(() => {
         animOnScroll()
     }, 200)
 }
+
+// span animation
+
+const container = document.querySelector(".welcome");
+const titleHello = container.querySelector("h1");
+const titleName = container.querySelector("h2");
+const titleProfession = container.querySelector("p");
+
+
+function createSpans(element) {
+    const text = element.textContent;
+    element.innerHTML = "";
+    for (let i = 0; i < text.length; i++) {
+        const span = document.createElement("span");
+        span.textContent = text[i];
+        element.appendChild(span);
+    }
+}
+
+createSpans(titleHello);
+createSpans(titleName);
+createSpans(titleProfession);
+
+function animSpan() {
+    let spans = container.querySelectorAll("span");
+    
+    for (let i = 0; i < spans.length; i++) {
+        const span = spans[i];
+        span.style.position = 'absolute'
+        span.style.transition = `all ${(rand(0, 10))/10}s ease ${(rand(0, 10))/10}s`;
+        span.style.top = `${rand(window.innerHeight, -window.innerHeight)}px`; //hImg*2, window.innerHeight/2
+        span.style.left = `${rand(-window.innerHeight, window.innerHeight)}px`;
+        span.style.transform = `rotate(${rand(-180, 180)}deg)`;
+    
+        setInterval(() => {
+            let index = 0
+            for (let y = 0; y < spans.length; y++) {
+                const span = spans[index]
+                index++
+                span.style.top = 0 + 'px'
+                span.style.left = 0 + 'px'
+                span.style.position = 'relative'
+            }
+        }, 100)
+    }
+}
+
+window.addEventListener('load', animSpan());
